eab58fb5ee3a8af9ed8d2afee7badeb25e8f82c9
Kovis97 <galgoczi.aron97@gmail.com>
2020-12-04 22:54:45 +0100

    Parameterezes es 'q'-ra valo kilepes a ciklusbol

--- kockadobas.1.2.py
+++ kockadobas.1.2.py
@@ -20,9 +20,9 @@ for i in range(12):
     I_k = I
 
     # kép átméretezése a vizsgálathoz, hogy a dobokocka pottyei kozel azonosak legyenek
-    # Ezzel csak azt lehet kiküszöbölni, hogy azonos távolságból, de más felbontással
+    # Ezzel szeretném kiküszöbölni, hogy azonos távolságból, de más felbontással
     # készült képeken is mukodjon
-    print(I.shape)
+    #print(I.shape)
 
     x = 2000 / I_k.shape[0]
     y = x
@@ -38,20 +38,20 @@ for i in range(12):
     # Morfológiai zárás. Az apró hibákat eltünteti az objektum felületén.
     I_median = cv2.morphologyEx(I_median, cv2.MORPH_CLOSE, k)
 
-    mini = 250
-    maxi = 255
+    #mini = 250
+    #maxi = 255
     #I_derivalt = cv2.Canny(I_median, mini, maxi)
 
     rows = I_median.shape[0]
     circles = cv2.HoughCircles(I_median, cv2.HOUGH_GRADIENT, 1, rows / 100,
-                            param1=245, param2=24 ,
+                            param1=240, param2=24 ,
                             minRadius=1, maxRadius=50)
     korok =0
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
             center = (i[0], i[1])
-            print(center, i[2])
+            #print(center, i[2])
             # circle center
             cv2.circle(I_k, center, 1, (255, 0, 0), 8)
             # circle outline
@@ -91,7 +91,7 @@ for i in range(12):
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
             center = (i[0], i[1])
-            print(center, i[2])
+            #print(center, i[2])
             # circle center
             cv2.circle(I_median, center, 1, (255, 0, 0), 1)
             # circle outline
@@ -106,6 +106,7 @@ for i in range(12):
     y = x
 
     I_k = cv2.resize(I_k, None, None, x, y, cv2.INTER_CUBIC)
+    
     x = 500 / I_median.shape[0]
     y = x
 
@@ -115,9 +116,10 @@ for i in range(12):
     text = 'Ossz.: ' + str(korok) + ' Kocka(k): ' + str(kocka)            
     cv2.putText(I_k,text,(10,30),0,1,(0,255,0), 4, cv2.LINE_AA)
 
-    cv2.imshow("detected circles", I_k)
     cv2.imshow("I_median", I_median)
-    cv2.waitKey(0)
+    cv2.imshow("detected circles", I_k)
+    if cv2.waitKey(0) == 113:
+        break
     cv2.destroyAllWindows()
 
 cv2.destroyAllWindows()
