0a59fbbf76b76ea7f1b9fbdf423b12719af70a79
Kovis97 <galgoczi.aron97@gmail.com>
2020-12-12 18:07:49 +0100

    Tesztelés
    -> Képek csoportosítása a 'kepek' mappába
    -> A könnyebb tesztelés érdekében függvényt keszítettem mely feldolgozza a képeket és 3 különböző fájlból hivható meg:
    -->Kockadobas.py: egy vagy összes kép megjelenítése
    --> teszt0.py: txt fajlba irja a helyes és mert erteket
    --> teszt1.py: txt faljba irja a mert értékeket
    -> vizsgalat.py: vizsgal() fg, tartalmazza

---
+++ teszt0.py
@@ -0,0 +1,40 @@
+'''
+Dobókocka számláló alkalmazás: Bármilyen fényviszony és háttér előtt
+képes legyen megszámlálni maximum 4 db dobókocka összértékét illetve
+a dobókockák darabszámát is jelezze ki.
+'''
+'''
+    A feladat megoldásánál abból indultam ki, hogy minden dobókocka
+    fehér
+'''
+from vizsgalat import vizsgal
+import numpy as np
+import cv2
+import os
+
+f = open('Eredmeny_mert.txt', 'w')
+b = open('Eredmeny_valos.txt', 'w')
+f.write('Kep neve \t Dobas ertek\tkocka szama\n')   
+b.write('Kep neve \t Dobas ertek\tkocka szama\n')
+
+dirname = './kepek'     # eleresi utvonal
+dirlist = os.scandir(dirname)   # fajlok, es mappak listazasa mappából
+for e in dirlist:   # vegig iteralunk a neveken
+    kep = dirname + '/'+ e.name # teljes eleres
+    kepe = cv2.haveImageReader(kep) #le ellenorizuk, hogy megfelel-e cv2-nek, nehogy egy mappa nevet kapjunk
+    if (not kepe):
+        print('\tEz nem kep')
+    else:
+        eredmeny = vizsgal(kep,1)   # az elso parameter a kepet adja at, a masodik a képek megjeleniteset engedelyezi
+        print(kep)     # eredmeny: kor, kocka, waitKey(0) erteke
+        f.write(str(eredmeny[0]) + '\t' + str(eredmeny[1]) + '\t' + str(eredmeny[2]) + '\n')
+        print('Adja meg a korok szamat: ')
+        korok = int(input())
+        print('Adja meg a kockak szamat: ')
+        kockak = int(input())
+        b.write(str(eredmeny[0]) + '\t' + str(korok) + '\t' + str(kockak) + '\n')
+        
+        
+        if eredmeny[3] == 113: # q -re kilepunk a kepek feldolgozasabol
+            break
+        cv2.destroyAllWindows()
