74a52eaa677169576802094db6b65f408a095926
Kovis97 <galgoczi.aron97@gmail.com>
2020-12-20 11:31:21 +0100

    teszt fajlok egybe gyurba

--- teszt.py
+++ teszt.py
@@ -12,14 +12,18 @@ import numpy as np
 import cv2
 import os
 
-print("Vannak-e uj kepek?")
-uj = input()
+print("Vannak-e uj kepek? I/H")
+valasz = input()
+if valasz == "I" or valasz == "i":
+    uj = 1
+else:
+    uj = 0
 
 f = open('Eredmeny_mert.txt', 'w')
 text = ("Kep neve \t Dobas ertek\tkocka szama\n")
 if uj:
-	b = open('Eredmeny_valos.txt', 'w')
-	b.write(text)
+    b = open('Eredmeny_valos.txt', 'w')
+    b.write(text)
 f.write(text)   
 
 
@@ -31,18 +35,20 @@ for e in dirlist:   # vegig iteralunk a neveken
     if (not kepe):
         print('\tEz nem kep')
     else:
-        megjelenjen = 1
+        ## -------
+        megjelenjen = 0 # Ezt kell átírni, ha ellenőrizni szeretnénk a program képi kimenetét változatlan adatbázis esetén.
+        ## -------
         if uj:
-        	megjelenjen = 1
+            megjelenjen = 1 ## Ezt semmi kép nem írható át!!
         eredmeny = vizsgal(kep,megjelenjen)   # az elso parameter a kepet adja at, a masodik a képek megjeleniteset engedelyezi
         print(kep)     # eredmeny: kor, kocka, waitKey(0) erteke
         f.write(str(eredmeny[0]) + '\t' + str(eredmeny[1]) + '\t' + str(eredmeny[2]) + '\n')
         if uj:
-        	print('Adja meg a korok szamat: ')
-        	korok = int(input())
-        	print('Adja meg a kockak szamat: ')
-        	kockak = int(input())
-        	b.write(str(eredmeny[0]) + '\t' + str(korok) + '\t' + str(kockak) + '\n')
+            print('Adja meg a korok szamat: ')
+            korok = int(input())
+            print('Adja meg a kockak szamat: ')
+            kockak = int(input())
+            b.write(str(eredmeny[0]) + '\t' + str(korok) + '\t' + str(kockak) + '\n')
         
         
         if eredmeny[3] == 113: # q -re kilepunk a kepek feldolgozasabol
